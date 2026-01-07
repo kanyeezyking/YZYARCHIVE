f23266a1365a5b67105aa8831c559c0b8665ac9b
kanyeezyking <angusshugg0@gmail.com>
2026-01-08 03:40:54 +1100

    Refactor clothes array formatting

--- data.js
+++ data.js
@@ -1,23 +1,20 @@
 const clothes = [
-    {
-      id: 1,
-      name: "SK-01",
-      image: "images/sk-01-white.jpeg",
-      color: "white",
-      type: "sock",
-      price: 20,
-      season: 10
-    },
-    {
-      id: 2,
-      name: "SK-01",
-      image: "images/sk-01-gray.jpeg",
-      color: "gray",
-      type: "sock",
-      price: 20,
-      season: 10
-    }
-  ];
-  
-
-
+  {
+    id: 1,
+    name: "SK-01",
+    image: "images/sk-01-white.jpeg",
+    color: "white",
+    type: "sock",
+    price: 20,
+    season: 10
+  },
+  {
+    id: 2,
+    name: "SK-01",
+    image: "images/sk-01-gray.jpeg",
+    color: "gray",
+    type: "sock",
+    price: 20,
+    season: 10
+  }
+];
