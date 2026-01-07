ef5a7714e6d436ac5af65b471bc80739917dc103
kanyeezyking <angusshugg0@gmail.com>
2026-01-08 03:18:22 +1100

    Add clothes array with sample clothing items

---
+++ data.js
@@ -0,0 +1,21 @@
+const clothes = [
+    {
+      id: 1,
+      name: "SK-01",
+      image: "images/sk-01-white.jpeg",
+      color: "White",
+      type: "Sock",
+      price: 20,
+      season: 10
+    },
+    {
+      id: 2,
+      name: "White Tee",
+      image: "images/sk-01-gray.jpeg",
+      color: "Gray",
+      type: "Sock",
+      price: 20,
+      season: 10
+    }
+  ];
+  
