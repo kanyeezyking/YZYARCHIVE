3d8ee6ad376698cc75a87d36b0ae8fe2b0b035fc
kanyeezyking <angusshugg0@gmail.com>
2026-01-08 16:30:03 +1100

    Update clothes array with new item and color change
    Added a new clothing item (SP-01) and changed color for SK-01.

--- data.js
+++ data.js
@@ -21,12 +21,22 @@ const clothes = [
     id: 3,
     name: "SK-01",
     image: "images/sk-01-black.jpeg",
-    color: "black",
+    color: "charcoal",
     type: "sock",
     price: 20,
     year: 2025
+  },
+  {
+    id: 4,
+    name: "SP-01",
+    image: "images/sp-01-white.jpeg",
+    color: "white",
+    type: "sweatpant",
+    price: 40,
+    year: 2025
   }
 ];
+
 
 
 
