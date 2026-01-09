4a39ebca1ad81cc255c0f240c1616fae30fc657e
kanyeezyking <angusshugg0@gmail.com>
2026-01-09 16:28:53 +1100

    Refactor DOM elements and modal logic

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const typeOptionsByCategory = {
   tops: ["tshirt", "longsleeve", "bodysuit", "tubetop", "tanktop"],
   bottoms: ["short", "leggings", "pants", "sweatpant"],
   outerwear: ["jacket", "hoodie", "windbreaker"],
-  accessories: ["hat", "sunglass", "backpack"],
+  accessories: ["hat", "sunglass", "backpack", "belt"],
   undergarments: ["sock", "underwear", "bra"]
 };
 
@@ -175,6 +175,7 @@ typeFilter.addEventListener("change", applyFilters);
 ===================== */
 
 renderItems(clothes);
+
 
 
 
