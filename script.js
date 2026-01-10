9278d548456d3f5c8c99a8c1354400a4fa37ca69
kanyeezyking <angusshugg0@gmail.com>
2026-01-10 18:03:47 +1100

    Refactor script.js to improve readability and structure

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const typeOptionsByCategory = {
   tops: ["tshirt", "longsleeve", "bodysuit", "tubetop", "tanktop"],
   bottoms: ["short", "leggings", "pants", "sweatpant"],
   outerwear: ["jacket", "hoodie", "windbreaker", "parka"],
-  accessories: ["hat", "sunglass", "backpack", "belt"],
+  accessories: ["hat", "sunglass", "backpack", "belt", "gloves"],
   undergarments: ["sock", "underwear", "bra"]
 };
 
@@ -175,6 +175,7 @@ typeFilter.addEventListener("change", applyFilters);
 ===================== */
 
 renderItems(clothes);
+
 
 
 
